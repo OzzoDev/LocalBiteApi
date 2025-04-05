c9536753c21d2134564768e3049e6c4d04ddcd8f
OzzoDev <oscar.burman005@gmail.com>
2025-04-05 11:34:40 +0200

    Created a mongodb session middleware

--- server.js
+++ server.js
@@ -3,15 +3,35 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import AuthRouter from "./routes/AuthRouter.js";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const SESSION_KEY = process.env.SESSION_KEY;
+const MONGO_URI = process.env.MONGO_CONNECT;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: MONGO_URI,
+    }),
+    cookie: { httpOnly: true, secure: false }, // Set secure: true in production
+  })
+);
+
+//how to use
+//req.session.jwt = jwt
 
 app.use("/auth", AuthRouter);
 
