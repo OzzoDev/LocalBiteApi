2f4b841e5053a0e598f2030488c11a190d1e11f4
oscar <oscar.burman005@gmail.com>
2025-04-20 20:38:43 +0200

    Attached validators middlewares to signup and signin routes

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { signin, signup, signout } from "../controllers/AuthController.js";
+import { validateLoginAttempt, validateNewUser } from "../validators/users.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/signup", validateNewUser, signup);
+router.post("/signin", validateLoginAttempt, signin);
 router.post("/signout", signout);
 
 export default router;
