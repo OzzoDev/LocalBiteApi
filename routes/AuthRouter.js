ffc7b5ae472bc6b2af7b01eb4504d3e601671b0b
oscar <oscar.burman005@gmail.com>
2025-04-21 15:08:01 +0200

    Validator to validate req.body when hitting /verify endpoint

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { signin, signup, signout, verify } from "../controllers/AuthController.js";
-import { validateLoginAttempt, validateNewUser } from "../validators/users.js";
+import {
+  validateLoginAttempt,
+  validateNewUser,
+  validateUserVerification,
+} from "../validators/users.js";
 
 const router = express.Router();
 
 router.post("/signup", validateNewUser, signup);
 router.post("/signin", validateLoginAttempt, signin);
-router.post("/verify", verify);
+router.post("/verify", validateUserVerification, verify);
 router.post("/signout", signout);
 
 export default router;
--- validators/users.js
+++ validators/users.js
@@ -33,6 +33,23 @@ const loginAttemptSchema = z
     path: ["username"],
   });
 
+const verifyUserSchema = z
+  .object({
+    username: z.string({ message: "Username is required" }).optional(),
+    email: z
+      .string({ message: "Email is required" })
+      .email({ message: "Invalid email format" })
+      .optional(),
+    otp: z
+      .string({ message: "Verification code is required" })
+      .nonempty({ message: "Verification code is required" }),
+  })
+  .strict()
+  .refine((data) => data.username || data.email, {
+    message: "Either username or email must be provided.",
+    path: ["username"],
+  });
+
 export const validateNewUser = (req, _, next) => {
   try {
     newUserSchema.parse(req.body);
@@ -51,6 +68,21 @@ export const validateNewUser = (req, _, next) => {
 export const validateLoginAttempt = (req, _, next) => {
   try {
     loginAttemptSchema.parse(req.body);
+    next();
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return next(
+        new RequestBodyValidationError(err.errors.map((error) => error.message).join(", "))
+      );
+    }
+
+    next(err);
+  }
+};
+
+export const validateUserVerification = (req, _, next) => {
+  try {
+    verifyUserSchema.parse(req.body);
     next();
   } catch (err) {
     if (err instanceof z.ZodError) {
