ce29637cde7df36bb4707c221b1f2cc71f5f647d
oscar <oscar.burman005@gmail.com>
2025-04-25 19:37:52 +0200

    Added also middleware to validate request body for updating a business review

--- validators/reviews.js
+++ validators/reviews.js
@@ -15,9 +15,40 @@ const businessReviewSchema = z
   })
   .strict();
 
+const businessUpdateReviewSchema = z
+  .object({
+    rating: z
+      .number({ message: "Rating must be a number" })
+      .min(1, { message: "Rating must be greater or equal to 1" })
+      .max(10, { message: "Rating must be less or equal to 10" })
+      .optional(),
+    review: z
+      .string({ message: "Review must be a string" })
+      .nonempty({ message: "Review is required" })
+      .min(10, { message: "Review must be at least 10 characters long" })
+      .max(500, { message: "Review must not be longer than 500 characters" })
+      .optional(),
+  })
+  .strict();
+
 export const validateBusinessReviewBody = (req, res, next) => {
   try {
     businessReviewSchema.parse(req.body);
+    next();
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return next(
+        new RequestBodyValidationError(err.errors.map((error) => error.message).join(", "))
+      );
+    }
+
+    next();
+  }
+};
+
+export const validateBusinessUpdateReviewBody = (req, res, next) => {
+  try {
+    businessUpdateReviewSchema.parse(req.body);
     next();
   } catch (err) {
     if (err instanceof z.ZodError) {
