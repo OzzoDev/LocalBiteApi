b9d7c04744527e98b50374a40ed1668e18212ba4
oscar <oscar.burman005@gmail.com>
2025-04-25 20:20:15 +0200

    Set up controller for handling CRUD operations for business reviews

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,19 +1,45 @@
-const reviewBusiness = (req, res, next) => {
+import {
+  addBusinessReview,
+  deleteBusinessReview,
+  updateBusinessReview,
+} from "../services/db/reveiws";
+
+export const reviewBusiness = async (req, res, next) => {
+  const { businessid: businessId } = req.params;
+  const { id: userId } = req.user;
+
   try {
+    await addBusinessReview({ userId, businessId, ...req.body });
+
+    res.status(201).json({ message: "Business review added sucessfully", success: true });
   } catch (err) {
     next(err);
   }
 };
 
-const editBusinessReview = (req, res, next) => {
+export const editBusinessReview = async (req, res, next) => {
+  const { businessid: businessId } = req.params;
+  const { id: userId } = req.user;
   try {
+    const review = await updateBusinessReview({ userId, businessId, ...req.body });
+
+    res
+      .status(200)
+      .json({ message: `Review with id '${review.id}' updated successfully`, success: true });
   } catch (err) {
     next(err);
   }
 };
 
-const deleteBusinessReview = (req, res, next) => {
+export const removeBusinessReview = async (req, res, next) => {
+  const { reviewid: reviewId } = req.params;
+  const { id: userId } = req.user;
   try {
+    await deleteBusinessReview(userId, reviewId);
+
+    res
+      .status(200)
+      .json({ message: `Review with id '${reviewId}' deleted successfully`, success: true });
   } catch (err) {
     next(err);
   }
