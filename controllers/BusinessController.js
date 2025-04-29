911e304e58b95cc25e2f8df7e32ed97d1cb87c8e
oscar <oscar.burman005@gmail.com>
2025-04-29 16:47:15 +0200

    Ensured location is only looked up if lat and lon is in the request query

--- controllers/BusinessController.js
+++ controllers/BusinessController.js
@@ -5,7 +5,11 @@ export const getBusinesses = async (req, res, next) => {
   const { lat: latitude, lon: longitude } = req.query;
 
   try {
-    const location = await getLocation(latitude, longitude);
+    let location;
+
+    if (latitude && longitude) {
+      location = await getLocation(latitude, longitude);
+    }
 
     const businesses = await findBusinesses(req.query, location || "");
     res.status(200).json({ businesses, success: true });
